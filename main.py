0c3571027faf7d8a753d0250aaa3c13726b07d1a
alliehsu <allie.chiayu.hsu@gmail.com>
2025-08-26 22:57:00 +0100

    combine resume and jd then prompt to gpt

--- main.py
+++ main.py
@@ -2,13 +2,37 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 
+from resume_reader import get_resume_path, read_resume
+from jd_reader import get_jd_path, read_jd
+
+
+# load API key
 load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAPI_API_KEY"))
 
-client = OpenAI(
-    api_key=os.getenv("OPENAPI_API_KEY")
-)
 
-prompt = "Ready to dive into the job board ocean with me?"
+# read resume and job description
+resume_path = get_resume_path()
+resume_text = read_resume(resume_path)
+
+jd_paths = get_jd_path()
+jd_text = read_jd(jd_paths)
+
+
+# Construct prompt
+prompt = f"""
+This is a candidate's resume along with multiple job descriptions.
+Please analyze the candidate's suitability, skill match, and potential gaps, then provide a brief summary and suitability rating for each position.
+
+[Resume]
+{resume_text}
+
+==============================
+"""
+
+for title, jd in jd_text.items():
+    prompt += f"\n\n[Job Title: {title}]\n\n{jd}\n-"
+
 
 completion = client.chat.completions.create(
     messages=[
@@ -18,7 +42,7 @@ completion = client.chat.completions.create(
         }
     ],
     model="gpt-4o",
-    max_tokens=250
+    max_tokens=1000
 )
 
 print(completion.choices[0].message.content)
